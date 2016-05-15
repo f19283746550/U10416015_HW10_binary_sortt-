3dac5b7500ac12c65fc6a3a5e6a03d0d69e4cad3
f19283746550 <f19283746550@yahoo.com.tw>
2016-05-15 23:48:39 +0800

    Add files via upload

---
+++ TreeNode.java
@@ -0,0 +1,11 @@
+class TreeNode {
+   int data;
+   TreeNode left;
+   TreeNode right;
+   // «Øºc¤l
+   
+   public TreeNode(int data) {
+      this.data = data;
+      this.left = this.right = null;
+   }
+}
